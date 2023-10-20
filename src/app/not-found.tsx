37586d3a125392bbda43a1884502dc57ec46341d
Liam <liam@fascinated.cc>
2023-10-20 22:06:27 +0100

    fix 404 page from throwing an error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,7 @@
 import Card from "@/components/Card";
 import Container from "@/components/Container";
-import { headers } from "next/headers";
 
 export default async function NotFound() {
-  const headersList = headers();
-  const domain = headersList.get("host");
   return (
     <Container>
       <Card className="flex h-full items-center justify-center">
