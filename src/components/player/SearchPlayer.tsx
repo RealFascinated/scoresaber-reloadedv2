b9c38846f4f448a2ef1354132d3497dbd08448ff
Lee <liam@fascinated.cc>
2023-10-23 20:17:20 +0000

    Update src/components/player/SearchPlayer.tsx

--- src/components/player/SearchPlayer.tsx
+++ src/components/player/SearchPlayer.tsx
@@ -72,7 +72,7 @@ export default function SearchPlayer() {
           <a
             key={player.id}
             className="flex min-w-[14rem] items-center gap-2 rounded-md p-2 transition-all hover:bg-gray-600"
-            href={`/player/${player.id}`}
+            href={`/player/${player.id}/scoresaber/top/1`}
           >
             <Avatar label="Account" size={40} url={player.profilePicture} />
 
