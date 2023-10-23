cb9c2973513c26f8f7c23c98a9a025856e98778a
Liam <liam@fascinated.cc>
2023-10-23 07:18:06 +0100

    update again

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -1,4 +1,5 @@
 import PlayerPage from "@/components/player/PlayerPage";
+import { ssrSettings } from "@/ssrSettings";
 import { ScoreSaberAPI } from "@/utils/scoresaber/api";
 import { Metadata } from "next";
 
@@ -16,14 +17,18 @@ export async function generateMetadata({
     };
   }
 
+  const description =
+    `View ${player.name}'s scores, top plays, and more.&#x0A;` +
+    `Rank: #${player.rank} (#${player.countryRank} - ${player.country})&#x0A;` +
+    `PP: ${player.pp}&#x0A;` +
+    `Play Count: ${player.scoreStats.totalPlayCount}`;
+
   return {
     title: `${player.name}`,
-    description:
-      `View ${player.name}'s scores, top plays, and more.&#x0A;` +
-      `Rank: #${player.rank} (#${player.countryRank} - ${player.country})&#x0A;` +
-      `PP: ${player.pp}&#x0A;` +
-      `Play Count: ${player.scoreStats.totalPlayCount}`,
-    twitter: {
+    description: description,
+    openGraph: {
+      title: `${ssrSettings.siteName} - ${player.name}`,
+      description: description,
       images: [
         {
           url: player.profilePicture,
