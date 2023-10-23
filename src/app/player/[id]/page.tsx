a3f85918b17a70dec9acba3347e64e940031fca0
Liam <liam@fascinated.cc>
2023-10-23 07:45:16 +0100

    update embed - i fixed thumbnails!!!!!!

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -21,6 +21,7 @@ export async function generateMetadata({
 
   const description = `
     View ${player.name}'s scores, top plays, and more.
+    
     Rank: #${player.rank} (#${formatNumber(
       player.countryRank,
     )} - ${normalizedRegionName(player.country)})
