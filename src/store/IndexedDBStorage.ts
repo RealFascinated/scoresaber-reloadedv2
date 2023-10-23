060cbd81cddf5cec629515f2df56241de20fffc7
Liam <liam@fascinated.cc>
2023-10-23 10:03:57 +0100

    rename IndexedDB store

--- src/store/IndexedDBStorage.ts
+++ src/store/IndexedDBStorage.ts
@@ -1,19 +1,22 @@
 "use client";
 
-import { del, get, set } from "idb-keyval";
+import { createStore, del, get, set } from "idb-keyval";
 import { StateStorage } from "zustand/middleware";
+
+const storage = createStore("scoresaber-reloaded", "storage");
 
 export const IDBStorage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
     //console.log(name, "has been retrieved");
-    return (await get(name)) || null;
+
+    return (await get(name, storage)) || null;
   },
   setItem: async (name: string, value: string): Promise<void> => {
     //console.log(name, "with value", value, "has been saved");
-    await set(name, value);
+    await set(name, value, storage);
   },
   removeItem: async (name: string): Promise<void> => {
     //console.log(name, "has been deleted");
-    await del(name);
+    await del(name, storage);
   },
 };
