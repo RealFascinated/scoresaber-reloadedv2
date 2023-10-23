59701260969ce6d292ed51fbba525e89268bb90f
Liam <liam@fascinated.cc>
2023-10-23 07:43:31 +0100

    pls thumbnail

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -2,6 +2,7 @@ import PlayerPage from "@/components/player/PlayerPage";
 import { ssrSettings } from "@/ssrSettings";
 import { formatNumber } from "@/utils/number";
 import { ScoreSaberAPI } from "@/utils/scoresaber/api";
+import { normalizedRegionName } from "@/utils/utils";
 import { Metadata } from "next";
 
 type Props = {
@@ -18,26 +19,29 @@ export async function generateMetadata({
     };
   }
 
-  const description =
-    `View ${player.name}'s scores, top plays, and more.&#x0A;` +
-    `Rank: #${player.rank} (#${formatNumber(
+  const description = `
+    View ${player.name}'s scores, top plays, and more.
+    Rank: #${player.rank} (#${formatNumber(
       player.countryRank,
-    )} - ${formatNumber(player.country)})&#x0A;` +
-    `PP: ${player.pp}&#x0A;` +
-    `Play Count: ${formatNumber(player.scoreStats.totalPlayCount)}`;
+    )} - ${normalizedRegionName(player.country)})
+    PP: ${player.pp}
+    Play Count: ${formatNumber(player.scoreStats.totalPlayCount)}`;
 
   return {
     title: `${player.name}`,
     description: `View ${player.name}'s scores, top plays, and more.`,
     openGraph: {
-      type: "article",
-      title: `${ssrSettings.siteName} - ${player.name}`,
+      siteName: ssrSettings.siteName,
+      title: `${player.name}`,
       description: description,
       images: [
         {
           url: player.profilePicture,
         },
       ],
+    },
+    twitter: {
+      card: "summary",
     },
   };
 }
