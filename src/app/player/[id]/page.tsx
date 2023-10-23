7c26da73b2d8e15b525c1ee27da933b9a55f11e7
Liam <liam@fascinated.cc>
2023-10-23 07:04:51 +0100

    update per player embed

--- src/app/player/[id]/page.tsx
+++ src/app/player/[id]/page.tsx
@@ -17,8 +17,13 @@ export async function generateMetadata({
   }
 
   return {
-    title: `${player.name} - Scoresaber Leaderboards`,
-    description: `View ${player.name}'s scores, top plays, and more.,&#x0A;test`,
+    title: `${player.name}`,
+    description: `
+    View ${player.name}'s scores, top plays, and more.&#x0A;
+    Rank: #${player.rank} (${player.countryRank} - ${player.country})&#x0A;
+    PP: ${player.pp}&#x0A;
+    Play Count: ${player.scoreStats.totalPlayCount}&#x0A;
+    `,
     openGraph: {
       images: [
         {
