b2c9ff412be724d122626990e940b9030ed21727
Liam <liam@fascinated.cc>
2023-10-23 07:45:41 +0100

    update site title

--- src/ssrSettings.ts
+++ src/ssrSettings.ts
@@ -1,5 +1,5 @@
 export const ssrSettings = {
-  siteName: "Scoresaber Reloaded",
+  siteName: "ScoreSaber Reloaded",
   siteUrl: "https://ssr.fascinated.cc",
   proxy: "https://proxy.fascinated.cc",
 };
