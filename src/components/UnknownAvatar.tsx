d7fe245036eb59b2d7d2377c2b7745f6ed8ca2a7
Liam <liam@fascinated.cc>
2023-10-28 00:00:48 +0100

    fix broken avatar on search page

--- src/components/UnknownAvatar.tsx
+++ src/components/UnknownAvatar.tsx
@@ -1,11 +1,7 @@
-type UnknownAvatarProps = {
-  size?: number;
-};
-
-export default function UnknownAvatar({ size = 150 }: UnknownAvatarProps) {
+export default function UnknownAvatar() {
   return (
     <div
-      className={`mb-4 mt-2 flex h-[${size}px] w-[${size}px] items-center justify-center rounded-full bg-gray-600`}
+      className={`mb-4 mt-2 flex h-[150px] w-[150px] items-center justify-center rounded-full bg-gray-600`}
     >
       <p className="text-9xl">?</p>
     </div>
