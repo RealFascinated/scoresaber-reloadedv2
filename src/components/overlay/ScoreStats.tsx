2fbc463264b5b96baeda33525cc33fb24bd7c995
Liam <liam@fascinated.cc>
2023-11-05 22:18:23 +0000

    feat(overlay): make raw score bold

--- src/components/overlay/ScoreStats.tsx
+++ src/components/overlay/ScoreStats.tsx
@@ -10,7 +10,7 @@ export default function ScoreStats() {
 
   return (
     <div className="flex flex-col pl-2">
-      <p className="text-2xl">{formatNumber(scoreStats.score)}</p>
+      <p className="text-2xl font-bold">{formatNumber(scoreStats.score)}</p>
       <p className="text-2xl">Combo: {formatNumber(scoreStats.combo)}</p>
       <p className="text-2xl">
         {scoreStats.rank} {scoreStats.accuracy.toFixed(2)}%
